5b7334dc347f1b7b4e695a1f31624729d981a4e5
Ranaco <ranasatyamraj@gmail.com>
2025-01-21 17:46:47 +0530

    udpate

--- lib/neon_circular_timer.dart
+++ lib/neon_circular_timer.dart
@@ -461,10 +461,13 @@ class CountDownController {
 
   /// This Method returns the **Current Time** of Countdown Timer
   /// Formated into the selected [TextFormat]
-  String getTime() {
-    return _state
-        ._getTime(_state._controller!.duration! * _state._controller!.value);
+String getTime() {
+  if (_state._controller?.isAnimating == true || _state._controller?.isCompleted == true) {
+    return _state._getTime(_state._controller!.duration! * _state._controller!.value);
+  } else {
+    return "00:00";
   }
+}
 
   /// This Method returns the **Current Time** of Countdown Timer
   /// in seconds
